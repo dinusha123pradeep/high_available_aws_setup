959df363e212ef6a35518cba147a4039cdec3669
Dinusha Marathunga <dinusha123pradeep@gmail.com>
2022-10-21 21:03:11 +0800

    Removed unwanted certificate

--- elb.tf
+++ elb.tf
@@ -17,10 +17,9 @@ module "dinusha_elb_http" {
     },
     {
       instance_port      = 8080
-      instance_protocol  = "http"
+      instance_protocol  = "HTTP"
       lb_port            = 8080
-      lb_protocol        = "http"
-      ssl_certificate_id = "arn:aws:acm:eu-west-1:235367859451:certificate/6c270328-2cd5-4b2d-8dfd-ae8d0004ad31"
+      lb_protocol        = "HTTP"
     },
   ]
 
