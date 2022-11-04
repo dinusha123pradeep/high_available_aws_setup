6560805264dac143c610abc688cff74d70b5bf40
Dinusha Marathunga <dinusha123pradeep@gmail.com>
2022-11-04 19:54:44 +0800

    Added database address into the output

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,19 @@
 output "alb-dns-name" {
   value = aws_elb.alb.dns_name
 }
+
+##########################
+# Output - AWS mysql database address
+##########################
+output "database_endpoint" {
+  description = "The endpoint of the database"
+  value       = aws_db_instance.rds.address
+}
+
+##########################
+# Output - AWS mysql database port
+##########################
+output "database_port" {
+  description = "The port of the database"
+  value       = aws_db_instance.rds.port
+}
